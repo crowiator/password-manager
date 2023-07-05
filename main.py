7a7eeba33ff5228ffd1223c967aa0e97c898bf97
crow <vrana.samuel@gmail.com>
2023-07-05 14:15:44 +0200

    find function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -38,26 +39,67 @@ def generate_password():
 def save_password_to_file():
 
     # Variables from inputs
-    webpage = website_input.get()
+    webpage = website_input.get().title()
     username = username_input.get()
     password = password_input.get()
+
+    # Dictionary
+    new_data = {
+        webpage : {
+            "email": username,
+            "password": password
+        }
+    }
 
     # Check if inputs are not empty
     if len(webpage) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Wrong input", message="Please don't leave any fields empty!")
     else:
-        # Check if inputs are correct
-        is_ok = messagebox.askokcancel(title=webpage, message=f"These are the details entered: \nEmail:{username} \nPassword:{password}\n is it okay to save?")
-        if is_ok:
-            # Write to file
-            f = open("passwords.txt", "a")
-            f.write(f"{webpage} ; {username} ; {password}\n")
-            f.close()
+        try:
+            # Save data in json format
+            with open("passwords.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+            # If file does not exist, crate and write into new file
+        except FileNotFoundError:
+            with open("passwords.json", "w") as data_file:
+                json.dump(new_data,data_file, indent=4)
+        else:
+            # Updating old data
+            data.update(new_data)
 
+            with open("passwords.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             # Clear all entries
             website_input.delete(0, END)
             username_input.delete(0, END)
             password_input.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD------------------------------- #
+def find_password():
+    webpage_key = website_input.get().title()
+    try:
+        with open("passwords.json", "r") as data_file:
+        # Reading old data
+            data = json.load(data_file)
+            page = data[webpage_key]
+
+    except FileNotFoundError:
+        messagebox.showerror("Key Error", "No Data File Found")
+
+    except KeyError:
+        messagebox.showerror("Key Error", "No webpages found")
+
+    else:
+        email_item = page["email"]
+        password_item = page["password"]
+        messagebox.showinfo(f"{page}", f"Email: {email_item}\n Password: {password_item}")
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -76,32 +118,36 @@ canvas.grid(row=0, column=1)
 
 # Website label
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky='W')
 
 # Email/Username label
 username_label = Label(text="Email/Username:")
-username_label.grid(row=2, column=0)
+username_label.grid(row=2, column=0, sticky='W')
 
 # Password label
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky='W')
 
 # Website input
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=21)
+website_input.grid(row=1, column=1, sticky='W')
 website_input.focus()
 
 # Email/Username input
 username_input = Entry(width=35)
-username_input.grid(row=2, column=1, columnspan=2)
+username_input.grid(row=2, column=1, columnspan=2, sticky='W')
 username_input.insert(0, "jack@gmail.com")
 # Password input
 password_input = Entry(width=21)
-password_input.grid(row=3, column=1)
+password_input.grid(row=3, column=1, sticky='W')
 
 # Generate Password Button
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
+generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
+generate_password_button.grid(row=3, column=2, sticky='NS')
+
+# Search Button
+search_button = Button(text="Search", width=14,  command=find_password)
+search_button.grid(row=1, column=2, sticky='NS')
 
 # Add Button
 add_button = Button(width=36, text="Add", command=save_password_to_file)
